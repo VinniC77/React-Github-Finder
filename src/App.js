473ec4abc667e19cc1c90f0d66121ee2197d7dc8
VinniC77 <vinniclunc@hotmail.com>
2022-10-01 11:57:10 -0300

    Added pages and tailwind settle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import About from './pages/About';
 
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+
+        <main className='contaier mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfounf" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+  {/* Se algém digitar uma rota que não existe, usamos o /* */}
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
