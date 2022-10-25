fca85ef84f2998526e34418931abdc8491c4a7ad
VinniC77 <vinniclunc@hotmail.com>
2022-10-24 23:27:47 -0300

    Context

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import About from './pages/About';
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
 
-        <main className='contaier mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notfounf" element={<NotFound />} />
-            <Route path="/*" element={<NotFound />} />
-  {/* Se algém digitar uma rota que não existe, usamos o /* */}
-          </Routes>
-        </main>
+          <main className="contaier mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfounf" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+              {/* Se algém digitar uma rota que não existe, usamos o /* */}
+            </Routes>
+          </main>
 
-        <Footer />
-      </div>
-    </Router>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,29 +1,15 @@
-import { useState, useEffect } from "react";
+import { useContext, useEffect } from "react";
 import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
 
 const UserResults = () => {
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
+    const { users, loading, fetchUsers } = useContext(GithubContext)
+
 
     useEffect(() => {
         fetchUsers()
-    }, []) // Sempre que a página carregar, dispara a função fetchUsers
-
-    const fetchUsers = async () => {
-        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-            headers: {
-                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-            }
-// Aqui fazemos um request para a URL do github com o usuário que o usuário quiser pesquisar sobre. E o segundo argumento, é o token que passamos atrvés de headers e setando o authorization de acordo. Assim enviando o token junto com a requisição            
-        })
-
-        const data = await response.json() // Aqui transformamos a informação no tipo JSON
-
-        console.log(data)
-
-        setUsers(data)
-        setLoading(false)
-    }
+    }) // Sempre que a página carregar, dispara a função fetchUsers
 
     console.log(users)
 
@@ -31,7 +17,7 @@ const UserResults = () => {
         return (
             <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
                 {users.map((user) => {
-                    return <h3>{user.login}</h3>
+                    return <UserItem key={user.id} user={user} /> 
                 })}
             </div>
         )
